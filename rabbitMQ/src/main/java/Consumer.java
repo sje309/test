baad9476aa58755d95ea29a74e40c6fed87c2431
shuyizhi <shuyizhi@zxisl.com>
2018-06-27 16:57:53 +0800

    RabbitMQ

---
+++ rabbitMQ/src/main/java/Consumer.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @Author: shuyizhi
+ * @Date: 2018-06-27 11:03
+ * @Description:
+ */
+public class Consumer {
+    public final static String QUEUE_NAME = "rabbitMQ_test2";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+    }
+}
