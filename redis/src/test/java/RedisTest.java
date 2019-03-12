63ab9988f46474378ab584a1bd1f2f2a9192e5a1
394604592 <394604592@qq.com>
2019-03-12 16:33:00 +0800

    redis 测试

--- redis/src/test/java/RedisTest.java
+++ redis/src/test/java/RedisTest.java
@@ -3,7 +3,9 @@ import org.junit.Test;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-/** @Author: shuyizhi @Date: 2018-10-26 11:14 @Description: */
+/**
+ * @Author: shuyizhi @Date: 2018-10-26 11:14 @Description:
+ */
 public class RedisTest {
     MySql mySql = new MySql();
     Redis redis = new Redis();
@@ -26,6 +28,29 @@ public class RedisTest {
                 System.out.println("Welcome MySql! User" + userName + "login success");
                 redis.hset("user_" + id, "username_", userName);
                 /** 设置过期时间 */
+                redis.expire("user_" + id, 30 * 60);
+            }
+        }
+    }
+
+    @Test
+    public void testLocal() throws SQLException {
+        int id = 4;
+        String sql = "select * from users where id=" + id;
+        String name = "";
+        if (redis.hexists("user_" + id, "user_name")) {
+            name = redis.hget("user_" + id, "username_");
+            System.out.println("Welcome Redis! user" + name + "login success!");
+        } else {
+            rs = mySql.connection.createStatement().executeQuery(sql);
+            if (rs.next() == false) {
+                System.out.println("MySql no register,Please register first");
+            } else {
+                name = rs.getString("name");
+                System.out.println("Welcome MySql! User" + name + " login success");
+                /**设置redis缓存*/
+                redis.hset("user_" + id, "username_", name);
+                /**设置过期时间*/
                 redis.expire("user_" + id, 30 * 60);
             }
         }
